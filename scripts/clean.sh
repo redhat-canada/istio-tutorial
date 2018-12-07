d376dbd54b39d91ab02073d153e2c1a3c5006ecf
msauve <martin.sauve@gmail.com>
2018-12-07 08:37:32 -0500

    refactor v2

--- scripts/clean.sh
+++
@@ -1,2 +0,0 @@
-oc delete -f ../istiofiles/destination-rule-recommendation-v1-v2.yml
-oc delete -f ../istiofiles/virtual-service-recommendation-v1.yml
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,10 @@
 QUERY=$(oc get virtualservice/customer -o jsonpath='{.spec.http[0].match[0].uri.exact}')
 GATEWAY_URL="http://$(oc get route istio-ingressgateway -n istio-system --template='{{ .spec.host }}')$QUERY"
 
+echo "ENDPOINT: $GATEWAY_URL"
+sleep 1
+
 while true
 do curl $GATEWAY_URL
-sleep .2
+sleep .4
 done
